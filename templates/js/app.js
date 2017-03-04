d9be0fa162031a64c677c4075c7bdee1a06a0954
Mikhail Kravets <22kravets22@mail.ru>
2017-03-04 12:36:07 +0200

    Mind up navigation

---
+++ templates/js/app.js
@@ -0,0 +1,22 @@
+function showNavigation(menuButtonElem, navigationElem){
+    menuButtonElem.on('click', function(e){
+        if(navigationElem.hasClass("navigationHidden")) {
+            navigationElem.removeClass("navigationHidden");
+            navigationElem.addClass("navigationExpanded");
+            menuButtonElem.removeClass("navigationButtonHidden");
+        }
+        else {
+//            angular.element("#moreIconsContainer").css("display", "none");
+//            angular.element("#showMoreIcons").removeClass("iconButtonExpanded");
+            
+            navigationElem.removeClass("navigationExpanded");
+            navigationElem.addClass("navigationHidden");
+            menuButtonElem.addClass("navigationButtonHidden");
+        }
+    });
+}
+
+
+window.onload = function(){
+    showNavigation($("#navigationButton"), $("#navigation"));
+}
