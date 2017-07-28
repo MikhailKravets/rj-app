ed14000c53a35843039ac5f0735a359fcdd31966
Mikhail Kravets <22kravets22@mail.ru>
2017-07-28 17:22:42 +0300

    Write orm for main package (users, disciplines, teachers tables)

---
+++ main/models.py
@@ -0,0 +1,34 @@
+import sqlalchemy
+from lib.databases import ModelBase
+
+
+Base = ModelBase()
+
+
+class User(Base):
+    __table_name__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    first_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    middle_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    last_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    sex = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    access = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    activated = sqlalchemy.Column(sqlalchemy.SmallInteger, nullable=False)
+
+
+class Discipline(Base):
+    __table_name = 'disciplines'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
+    feature = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    cycle = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    code = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+class Teacher(Base):
+    __table_name = 'teachers'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
