5f7e8002979be4a817f6e25d2bdf3cc5d68ffdb9
Mikhail Kravets <22kravets22@mail.ru>
2017-07-28 21:52:07 +0300

    Update main model: remove redundant autoincrement

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ Base = ModelBase()
 
 class User(Base):
     __table_name__ = 'users'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     first_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -21,7 +21,7 @@ class User(Base):
 
 class Discipline(Base):
     __table_name = 'disciplines'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     feature = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     cycle = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -30,5 +30,5 @@ class Discipline(Base):
 
 class Teacher(Base):
     __table_name = 'teachers'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
