6e0c4cd2951238342fea8c5319374f2b6b84450c
Mikhail Kravets <22kravets22@mail.ru>
2017-07-29 12:57:02 +0300

    Update model

--- main/models.py
+++ main/models.py
@@ -8,24 +8,24 @@ Base = ModelBase()
 class User(Base):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    first_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    middle_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    last_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    sex = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    access = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    activated = sqlalchemy.Column(sqlalchemy.SmallInteger, nullable=False)
+    login = sqlalchemy.Column(sqlalchemy.String(85), nullable=False)
+    password = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    first_name = sqlalchemy.Column(sqlalchemy.String(85), nullable=False)
+    middle_name = sqlalchemy.Column(sqlalchemy.String(85), nullable=True)
+    last_name = sqlalchemy.Column(sqlalchemy.String(85), nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
+    sex = sqlalchemy.Column(sqlalchemy.String(1), nullable=False, server_default='M')
+    access = sqlalchemy.Column(sqlalchemy.String(10), nullable=False, server_default='1')
+    activated = sqlalchemy.Column(sqlalchemy.SmallInteger, nullable=False, server_default='1')
 
 
 class Discipline(Base):
     __tablename__ = 'disciplines'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
-    feature = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    cycle = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    code = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(255), nullable=False, unique=True)
+    feature = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    cycle = sqlalchemy.Column(sqlalchemy.String(85), nullable=False)
+    code = sqlalchemy.Column(sqlalchemy.String(25), nullable=False)
 
 
 class Teacher(Base):
