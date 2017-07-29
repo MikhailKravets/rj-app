40daa6321d6330907eca15bd450fa7c8bffab90d
Mikhail Kravets <22kravets22@mail.ru>
2017-07-29 12:38:25 +0300

    Fix models

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ Base = ModelBase()
 
 
 class User(Base):
-    __table_name__ = 'users'
+    __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -20,7 +20,7 @@ class User(Base):
 
 
 class Discipline(Base):
-    __table_name = 'disciplines'
+    __tablename__ = 'disciplines'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     feature = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -29,6 +29,6 @@ class Discipline(Base):
 
 
 class Teacher(Base):
-    __table_name = 'teachers'
+    __tablename__ = 'teachers'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
