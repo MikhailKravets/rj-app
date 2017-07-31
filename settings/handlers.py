8ddd88e74b3a007b8986d8377c928f4f2c510913
Mikhail Kravets <22kravets22@mail.ru>
2017-07-31 13:07:27 +0300

    Refactor settings handlers

--- settings/handlers.py
+++ settings/handlers.py
@@ -1,24 +1,26 @@
+import json
+import logging
+import config
+import lib.decorators as decorator
 import tornado.web as web
-
-from config import *
 
 
 class SettingsHandler(web.RequestHandler):
-    @Decorator.authorized
-    @Decorator.inline
+    @decorator.authorized(access_level='1')
+    @decorator.inline
     async def get(self):
-        user = Config.users[self.get_cookie('session')]
-        self.render('settings.html', login=user.login,
-                    last=user.last, middle=user.middle, first=user.first,
-                    email=user.email)
+        # TODO: ask Mysql for user's data
+        self.render('settings.html', login='',
+                    last='', middle='', first='',
+                    email='')
 
-    @Decorator.authorized
+    @decorator.authorized
     async def post(self):
         data = json.loads(self.request.body)
         if data[0] == 'UPDATE':
-            retr = Config.users[self.get_cookie('session')].update_settings(data[1])
-            self.write(json.dumps(retr))
+            # TODO: invent normal client/server speaking protocol
+            pass
         self.finish()
 
     def get_template_path(self):
-        return Config.TEMPLATE_PATH
+        return config.TEMPLATE_PATH
