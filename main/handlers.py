f3c818631819b016b31ed67c300abfd6f85c9e99
Mikhail Kravets <22kravets22@mail.ru>
2017-07-31 13:04:47 +0300

    Refactor main handlers

--- main/handlers.py
+++ main/handlers.py
@@ -1,22 +1,21 @@
+import json
+import logging
+import config
+import lib.decorators as decorator
 import tornado.web as web
-
-from config import *
 
 
 class MainHandler(web.RequestHandler):
-    @Decorator.authorized
+    @decorator.authorized
     async def get(self):
-        self.render('main.html', access=Config.users[self.get_cookie('session')].access)
+        self.render('main.html', access=self.session['access'])
 
     async def post(self):
         data = json.loads(self.request.body)
         if data[0] == 'QUIT':
-            try:
-                del Config.users[self.get_cookie('session')]
-            except KeyError:
-                logging.debug('User is already quited')
+            # TODO: make client/server speaking protocol
             self.write('OK')
         self.finish()
 
     def get_template_path(self):
-        return Config.TEMPLATE_PATH
+        return config.TEMPLATE_PATH
