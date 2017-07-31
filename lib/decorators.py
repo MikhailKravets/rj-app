9476f2e3adb06f32e0593c64f0f46b8a68f49a39
Mikhail Kravets <22kravets22@mail.ru>
2017-07-31 12:56:42 +0300

    Refactor auth decorator again

--- lib/decorators.py
+++ lib/decorators.py
@@ -1,23 +1,27 @@
 from lib.session import HashSession
 
 
-def authorized(fn):
-    async def wrapper(self, *args):
-        redis_session = HashSession()
-        key = self.get_cookie('session')
-        if key not in redis_session:
+def authorized(access_level='1'):
+    async def higher_wrapper(fn):
+        def write(self):
             inline = self.get_argument('inline', False)
             if inline == '1':
                 self.write('DENIED')
             else:
                 self.redirect('/auth')
 
-        else:
-            self.session = redis_session[key]
-            if self.session['activated'] == '0':
-                self.redirect('/auth')
-            await fn(self, *args)
-    return wrapper
+        async def wrapper(self, *args):
+            redis_session = HashSession()
+            key = self.get_cookie('session')
+            if key not in redis_session:
+                write(self)
+            else:
+                self.session = redis_session[key]
+                if self.session['activated'] == '0' and access_level not in self.session['access']:
+                    write(self)
+                await fn(self, *args)
+        return wrapper
+    return higher_wrapper
 
 
 def inline(fn):
